17f6d813b10b2c93584a37dd2c2c42fe43e3c3a4
= <=>
2023-11-29 06:47:59 +0200

    Seems to be working. Fetched top 10 task lists

--- tasks-api-client/src/client.py
+++ tasks-api-client/src/client.py
@@ -1,19 +1,24 @@
+import json
 import os.path
+import string
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from op import get_secure_note_field, run_op_command
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/tasks.readonly"]
+OP_VAULT = "Google API"
+OP_ITEM = "Google Tasks API"
 
 
 def main():
     """Shows basic usage of the Tasks API.
-  Prints the title and ID of the first 10 task lists.
-  """
+    Prints the title and ID of the first 10 task lists.
+    """
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -24,11 +29,13 @@ def main():
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
-    else:
-        flow = InstalledAppFlow.from_client_secrets_file(
-            "credentials.json", SCOPES
-        )
-        creds = flow.run_local_server(port=0)
+        else:
+            run_op_command(["op", "signin"])
+            secrets_json = get_secure_note_field(OP_VAULT, OP_ITEM, "desktop-credentials.json")
+            client_config = json.loads(secrets_json)
+            flow = InstalledAppFlow.from_client_config(client_config, SCOPES)
+            creds = flow.run_local_server(port=0)
+
     # Save the credentials for the next run
     with open("token.json", "w") as token:
         token.write(creds.to_json())
@@ -51,5 +58,5 @@ def main():
         print(err)
 
 
-        if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
