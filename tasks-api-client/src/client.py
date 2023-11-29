172f418dafde2d602a48e53a6ca63cf960c8a832
= <=>
2023-11-29 08:05:40 +0200

    Fetches lists

--- tasks-api-client/src/client.py
+++ tasks-api-client/src/client.py
@@ -9,6 +9,7 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from op import get_secure_note_field, run_op_command
 
+
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/tasks.readonly"]
 OP_VAULT = "Google API"
@@ -19,17 +20,30 @@ def main():
     """Shows basic usage of the Tasks API.
     Prints the title and ID of the first 10 task lists.
     """
+
+    lists = get_task_lists()
+    print("Task lists:")
+    for list in lists:
+        print(f"{list['title']}: ({list['etag']})")
+
+
+def get_credentials():
+    """ Get credentials for Google Tasks API """
+
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
+    # TODO Store token in 1Password
     if os.path.exists("token.json"):
         creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
+            # TODO How to do this without human interaction?
             run_op_command(["op", "signin"])
             secrets_json = get_secure_note_field(OP_VAULT, OP_ITEM, "desktop-credentials.json")
             client_config = json.loads(secrets_json)
@@ -40,22 +54,29 @@ def main():
     with open("token.json", "w") as token:
         token.write(creds.to_json())
 
-    try:
-        service = build("tasks", "v1", credentials=creds)
+    return creds
 
-        # Call the Tasks API
-        results = service.tasklists().list(maxResults=10).execute()
-        items = results.get("items", [])
 
-        if not items:
-            print("No task lists found.")
-            return
+def get_task_service():
+    """ Get a Google Tasks API service object """
 
-        print("Task lists:")
-        for item in items:
-            print(f"{item['title']} ({item['id']})")
-    except HttpError as err:
-        print(err)
+    creds = get_credentials()
+    service = build("tasks", "v1", credentials=creds)
+    return service
+
+
+def get_task_lists(page_size=10, filter: string = None):
+    """ Get a list of task lists """
+    
+    service = get_task_service()
+    results = service.tasklists().list(maxResults=page_size).execute()
+    items = results.get("items", [])
+
+    if not items:
+        print("No task lists found.")
+        return
+    
+    return items
 
 
 if __name__ == "__main__":
